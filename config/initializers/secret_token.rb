6670703a9b34c2de3f1908e0f05080fed86af251
Dominik Guzei <dominik_guzei@gmx.at>
2011-10-02 21:29:55 -0400

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NerdComposer::Application.config.secret_token = '28418acd0aebf3f47b95d1b639719dae0dbf990fe38ebac6d3ac7c9ecb50a618b09df39676ff7da4ada84ef4cbf5c47019a6c703ccfa9186cc35823511cb5ea8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+NerdComposer::Application.config.session_store :cookie_store, key: '_nerd_composer_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# NerdComposer::Application.config.session_store :active_record_store
