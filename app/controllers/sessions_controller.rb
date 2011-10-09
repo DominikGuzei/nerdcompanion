5532769a09a3e3ce0682eea006755d78d7b13b01
Dominik Guzei <dominik_guzei@gmx.at>
2011-10-09 14:23:20 -0400

    Removes unused user email requirement

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,11 +9,7 @@ class SessionsController < ApplicationController
     user = User.where(:provider => auth['provider'], 
                       :uid => auth['uid']).first || User.create_with_omniauth(auth)
     session[:user_id] = user.id
-    if !user.email
-      redirect_to edit_user_path(user), :alert => "Please enter your email address."
-    else
-      redirect_to home_path, :notice => 'Signed in!'
-    end
+    redirect_to home_path, :notice => 'Signed in!'
 
   end
 
